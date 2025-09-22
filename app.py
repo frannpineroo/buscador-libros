bc624562cf42e249c76d5d4009170ed17cd45f78
frannpineroo <frannpinero@hotmail.com>
2025-09-22 16:03:05 -0300

    Primer commit: proyecto buscador de libros

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+# Ruta principal
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    books = []
+    total = 0
+    query = ""
+
+    if request.method == 'POST':
+        query = request.form.get('search')
+        url = f"https://openlibrary.org/search.json?q={query}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            total = data.get('numFound', 0)
+            books = data.get('docs', [])[:10]  # Limitar a los primeros 10 resultados
+    return render_template('index.html', books=books, total=total, query=query)
+
+if __name__ == '__main__':
+    app.run(debug=True)
